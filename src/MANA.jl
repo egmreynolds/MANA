c898f459fc8b2453fb4f293639e922717c433f3f
egmreynolds <edrey0@livestock.org.nz>
2020-08-13 18:40:07 +1200

    Pkg improvements

--- src/MANA.jl
+++ src/MANA.jl
@@ -1,5 +1,6 @@
+module MANA
 # Packge Main for MANA GWAS package
-using Distributed, DelimitedFiles, Statistics, BGZFStreams, LinearAlgebra, Random, SparseArrays, SharedArrays, BEDFiles, Distributions
+using Distributed, DelimitedFiles, Statistics, LinearAlgebra, Random, SparseArrays, BEDFiles, Distributions
 using JuliaDB, IndexedTables, OnlineStats, JWAS, DataFrames
 #
 include("types.jl")
@@ -10,28 +11,14 @@ include("run/run.jl")
 include("summarise/summarise.jl")
 include("ycreator/ycreator.jl")
 include("pedigree/pedigree.jl")
-#
-export build_gwas
-export add_snps
-export setup_phenotypes
-export initiate_model
-export run_model
-export concatenate_summaries
-#
-export make_alphas
-export filter_alphas
-export print_alphas
-#
-export build_ycreator
-export add_phenotypes
-export add_alphas
-export add_markers_gensel
-export sort_ycreator
-export initiate_ycreator
-export create_yk_loso
-export create_yk_full
-export run_2locus_model
-export setup_phenotypes_full
-export run_pedigree_model
-export run_spliceQTL 
-export run_pedigree_model_multirecord
+
+# Functions for basic model
+export build_gwas, add_snps, setup_phenotypes, initiate_model, run_model, concatenate_summaries,
+# Functions for adjusting marker effects
+export make_alphas, filter_alphas, print_alphas
+# Functions for adjusting phenotypes
+export build_ycreator, add_phenotypes, add_alphas, add_markers_gensel, sort_ycreator, initiate_ycreator, create_yk_loso
+# Functions for pedigree models
+export run_pedigree_model, run_spliceQTL, run_pedigree_model_multirecord
+
+end
