f9d48b038909515c483df49a9b8551c66c26ad89
egmr <edwardoreynolds@gmail.com>
2022-01-27 14:43:55 +1300

    Update summarise_base.jl

--- src/summarise/summarise_base.jl
+++ src/summarise/summarise_base.jl
@@ -1,16 +1,19 @@
 #Functions used to summarise GWAS outputs
 function calc_summaries(submodel)
-    calc_tstats!(submodel)
+    calc_zstats!(submodel)
     calc_pvalues_norm!(submodel)
 end
 
-function calc_tstats!(submodel)
+function calc_zstats!(submodel)
     idx = findall(x -> x != -9, submodel.output_stats[:,1]) # all where MAF != -9
     if submodel.type == "additive"
         submodel.output_stats[idx,5] = submodel.output_stats[idx,3] ./ sqrt.(submodel.output_stats[idx,4])
     else
         submodel.output_stats[idx,7] = submodel.output_stats[idx,3] ./ sqrt.(submodel.output_stats[idx,4])
         submodel.output_stats[idx,8] = submodel.output_stats[idx,5] ./ sqrt.(submodel.output_stats[idx,6])
+	submodel.output_stats[idx,14] = submodel.output_stats[idx,12] ./ sqrt.(submodel.output_stats[idx,13])
+        submodel.output_stats[idx,20] = submodel.output_stats[idx,16] ./ sqrt.(submodel.output_stats[idx,17])
+        submodel.output_stats[idx,21] = submodel.output_stats[idx,18] ./ sqrt.(submodel.output_stats[idx,19])
     end
 end
 
@@ -22,6 +25,9 @@ function calc_pvalues_norm!(submodel)
     else
         submodel.output_stats[idx,9] = 2*ccdf.(dist, abs.(submodel.output_stats[idx,7]))
         submodel.output_stats[idx,10] = 2*ccdf.(dist, abs.(submodel.output_stats[idx,8]))
+	submodel.output_stats[idx,15] = 2*ccdf.(dist, abs.(submodel.output_stats[idx,14]))
+        submodel.output_stats[idx,22] = 2*ccdf.(dist, abs.(submodel.output_stats[idx,20]))
+        submodel.output_stats[idx,23] = 2*ccdf.(dist, abs.(submodel.output_stats[idx,21]))
     end
 end
 
@@ -32,9 +38,9 @@ function write_summaries(submodel)
 end
 
 function concatenate_summaries(model)
-    fulloutput = Any["CHR" "BP" "SNP" "REF" "ALT" "MAF" "K" "BETA_A" "VAR_A" "BETA_D" "VAR_D" "T_A" "T_D" "P_A" "P_D"] # may need a check
+    fulloutput = Any["CHR" "BP" "SNP" "REF" "ALT" "MAF" "K" "BETA_A" "VAR_A" "BETA_D" "VAR_D" "Z_A" "Z_D" "P_A" "P_D" "BETA_I" "BETA_ALPHA" "VAR_ALPHA" "Z_ALPHA" "P_ALPHA" "BETA_HET" "VAR_HET" "BETA_HOM" "VAR_HOM" "Z_HET" "Z_HOM" "P_HET" "P_HOM"] 
     if model.type == "additive"
-        fulloutput = Any["CHR" "BP" "SNP" "REF" "ALT" "MAF" "K" "BETA_A" "VAR_A" "T_A" "P_A"] # may
+        fulloutput = Any["CHR" "BP" "SNP" "REF" "ALT" "MAF" "K" "BETA_A" "VAR_A" "Z_A" "P_A"] # may
     end
     for future in model.futures
         wait(future)
